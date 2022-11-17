9861ec22551c9712fb8cede4a700a767086bb7ef
justin <justinonomo@gmail.com>
2022-11-17 12:43:08 +0100

    Make the counter works

--- wbu-atomique-theme/src/js/sections/count-the-days.js
+++ wbu-atomique-theme/src/js/sections/count-the-days.js
@@ -1,1 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/vixon/vixon-counter-section.scss";
+import '@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/vixon/js/counter.js'
